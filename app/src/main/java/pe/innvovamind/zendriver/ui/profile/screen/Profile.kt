4669d2b827f88cd1be4dfbf22033e3173785a5dc
Frank_Alva <u202020026@upc.edu.pe>
2023-05-04 01:08:36 -0500

    feat(Profile): added profileInformation

--- app/src/main/java/pe/innvovamind/zendriver/ui/profile/screen/Profile.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/profile/screen/Profile.kt
@@ -9,15 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -82,5 +86,72 @@ fun ImageProfile(){
         }
         Text(text = "Henry Turrones")
         Text(text = "\nExperience as a RRHH in driving with a semi-trailer, trailer and coupled with the guarantee of safety and the driving experience that this entails.")
+    }
+}
+
+
+@Composable
+fun ProfileInformation(){
+    var email by rememberSaveable {
+        mutableStateOf("henry.antonio@gmail.com")
+    }
+    var phone by rememberSaveable {
+        mutableStateOf("(+51) 983 543 439")
+    }
+    var address by rememberSaveable {
+        mutableStateOf("Lima, Lince")
+    }
+    var facebook by rememberSaveable {
+        mutableStateOf("@Maconsa")
+    }
+    var instagram by rememberSaveable {
+        mutableStateOf("@Maconsa")
+    }
+    var twitter by rememberSaveable {
+        mutableStateOf("@Maconsa")
+    }
+
+
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Email", modifier = Modifier.width(100.dp))
+        TextField(value = email, onValueChange = {email = it} )
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Phone", modifier = Modifier.width(100.dp))
+        TextField(value = phone, onValueChange = {phone = it} )
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Address", modifier = Modifier.width(100.dp))
+        TextField(value = address, onValueChange = {address = it} )
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Facebook", modifier = Modifier.width(100.dp))
+        TextField(value = facebook, onValueChange = {instagram = it} )
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Instagram", modifier = Modifier.width(100.dp))
+        TextField(value = instagram, onValueChange = {instagram = it} )
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 4.dp, end = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Twitter", modifier = Modifier.width(100.dp))
+        TextField(value = twitter, onValueChange = {twitter = it} )
     }
 }
