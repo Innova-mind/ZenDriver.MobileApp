28bff2b43477d3ae4fd524cdb3177c5db3cbc365
dominikmendoza <u20201b980@upc.edu.pe>
2023-05-03 21:46:50 -0500

    feat(Sign in): Methods for viewModel

--- app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignInViewModel.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignInViewModel.kt
@@ -1,8 +1,10 @@
 package pe.innvovamind.zendriver.ui.login.screens.signin
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 
 class SignInViewModel : ViewModel() {
     private val _email = MutableLiveData<String>()
@@ -22,4 +24,24 @@ class SignInViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    fun onLoginChanged(email: String, password: String) {
+        _email.value = email
+        _password.value = password
+        _loginEnabled.value = isValidEmail(email) && isValidPassword(password)
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
+    }
+
+    suspend fun onLoginClicked() {
+        _isLoading.value = true
+        delay(2000)
+        _isLoading.value = false
+    }
 }
