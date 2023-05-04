aaea3bc300dc35dc8a94ddfd3e09fdbad3167a52
dominikmendoza <u20201b980@upc.edu.pe>
2023-05-03 21:53:43 -0500

    feat(Sign in): Sign in screen

--- app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignIn.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignIn.kt
@@ -1,2 +1,169 @@
 package pe.innvovamind.zendriver.ui.login.screens.signin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import pe.innvovamind.zendriver.R
+
+@Composable
+fun LoginScreen(viewModel: SignInViewModel) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        SignIn(Modifier.align(Alignment.Center), viewModel)
+    }
+}
+
+@Composable
+fun SignIn(modifier: Modifier, viewModel: SignInViewModel) {
+    val email :String by viewModel.email.observeAsState(initial = "")
+    val password :String by viewModel.password.observeAsState(initial = "")
+    val loginEnabled :Boolean by viewModel.loginEnabled.observeAsState(initial = false)
+    val isLoading :Boolean by viewModel.isLoading.observeAsState(initial = false)
+    val coroutineScope = rememberCoroutineScope()
+
+    if (isLoading) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    } else {
+        Column(modifier = modifier) {
+            HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField(email) { viewModel.onLoginChanged(it, password) }
+            Spacer(modifier = Modifier.padding(8.dp))
+            PasswordField(password) { viewModel.onLoginChanged(email, it) }
+            Spacer(modifier = Modifier.padding(8.dp))
+            ForgotPassword(Modifier.align(Alignment.End))
+            Spacer(modifier = Modifier.padding(8.dp))
+            SignInButton(loginEnabled) {
+                coroutineScope.launch {
+                    viewModel.onLoginClicked()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SignInButton(loginEnabled: Boolean, onLoginClicked: () -> Unit) {
+    Button(
+        onClick = { onLoginClicked() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF02AAEE),
+            disabledBackgroundColor = Color(0xFF47535E),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        ),
+        enabled = loginEnabled
+
+    ) {
+        Text(text = "Sign in")
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot password?",
+        modifier = modifier.clickable {  },
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF02AAEE)
+    )
+}
+
+@Composable
+fun PasswordField(password: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = password,
+        onValueChange = {onTextFieldChanged(it)},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {
+            Text(
+                text = "Password",
+                color = Color.Black
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFF000000),
+            backgroundColor = Color(0xFF02AAEE),
+            //focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        visualTransformation = PasswordVisualTransformation('*')
+    )
+}
+
+@Composable
+fun EmailField(email: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = email,
+        onValueChange = {onTextFieldChanged(it)},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {
+            Text(
+                text = "Email",
+                color = Color.Black
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFF000000),
+            backgroundColor = Color(0xFF02AAEE),
+            //focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logozendriver),
+        contentDescription = "Logo",
+        modifier = modifier
+    )
+}
+
