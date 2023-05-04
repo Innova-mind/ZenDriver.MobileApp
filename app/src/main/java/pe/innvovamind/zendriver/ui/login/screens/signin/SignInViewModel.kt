eda4ade4da2ce13b99d863811285ddaa50dc1796
dominikmendoza <u20201b980@upc.edu.pe>
2023-05-03 21:41:44 -0500

    feat(Sign in): Val's for SignInViewModel

--- app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignInViewModel.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/login/screens/signin/SignInViewModel.kt
@@ -1,4 +1,25 @@
 package pe.innvovamind.zendriver.ui.login.screens.signin
 
-class SignInViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+
+    private val _navigateToSignUp = MutableLiveData<Boolean>()
+    val navigateToSignUp: LiveData<Boolean> = _navigateToSignUp
+
+    private val _navigateToHome = MutableLiveData<Boolean>()
+    val navigateToHome: LiveData<Boolean> = _navigateToHome
+
+    private val _loginEnabled = MutableLiveData<Boolean>()
+    val loginEnabled: LiveData<Boolean> = _loginEnabled
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 }
