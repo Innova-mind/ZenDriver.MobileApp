40be582e5ce7fc79a3b5e239e3147218da0f797b
Nicole Price <u201921559@upc.edu.pe>
2023-05-15 22:21:38 -0500

    style(Message): Added input to write message and send it

--- app/src/main/java/pe/innvovamind/zendriver/ui/message/screens/Message.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/message/screens/Message.kt
@@ -1,18 +1,21 @@
 package pe.innvovamind.zendriver.ui.message.screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import pe.innvovamind.zendriver.ui.message.data.remote.MessageResponse
 
@@ -32,6 +35,8 @@ fun MessageDetail(messageList: SnapshotStateList<MessageResponse>, onBack: () ->
     messageList.add(MessageResponse(3,
         "Me gustaría recibir una actualización de su situación actual, para poder " +
                 "considerarlo en un futuro.","","",1,2))
+
+    var messageToSend by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.fillMaxSize()) {
         BackButton(onClick = {
@@ -63,11 +68,67 @@ fun MessageDetail(messageList: SnapshotStateList<MessageResponse>, onBack: () ->
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextField(
+                    modifier = Modifier.weight(1f),
+                    value = messageToSend,
+                    onValueChange = { messageToSend = it },
+                    textStyle = MaterialTheme.typography.body1,
+                    placeholder = { Text(text = "Send a message...") },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.White,
+                        cursorColor = MaterialTheme.colors.primary
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Send,
+                        keyboardType = KeyboardType.Text
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSend = {
+                            sendMessage(messageList, messageToSend)
+                            messageToSend = ""
+                        }
+                    )
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                IconButton(
+                    onClick = {
+                        sendMessage(messageList, messageToSend)
+                        messageToSend = ""
+                    },
+                    enabled = messageToSend.isNotBlank()
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Send,
+                        contentDescription = "Send"
+                    )
+                }
+            }
+        }
     }
-
-
 }
 
+private fun sendMessage(
+    messageList: SnapshotStateList<MessageResponse>,
+    message: String
+) {
+    if (message.isNotBlank()) {
+        messageList.add(MessageResponse(
+            messageList.size + 1,
+            message,
+            "",
+            "",
+            1,
+            2
+        ))
+    }
+}
 @Composable
 fun BackButton(onClick: () -> Unit) {
     IconButton(onClick = onClick, modifier = Modifier.padding(8.dp)) {
--- app/src/main/java/pe/innvovamind/zendriver/ui/message/screens/Messages.kt
+++ app/src/main/java/pe/innvovamind/zendriver/ui/message/screens/Messages.kt
@@ -1,6 +1,5 @@
 package pe.innvovamind.zendriver.ui.message.screens
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -13,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
